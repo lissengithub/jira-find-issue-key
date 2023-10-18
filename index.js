d5cf6c909efcf1eb7fd474a2dcbefcfd44e62d17
erik-lissen <111526841+erik-lissen@users.noreply.github.com>
2023-10-18 14:48:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function extractJiraKeysFromCommit() {
     // temporary use this method instead of debugging the old one
     const prefix = "LSN"; // TODO remove this default value
     const result = allMatches(prefix, await findStrings());
-    const resultStr = Array.from(result).join(",");
+    const resultStr = Array.from(result)?.[0];
     console.log('Result:',resultStr);
     core.setOutput("jira-keys", resultStr);
     // try {
